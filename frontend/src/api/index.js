e0b700d278d240bb4956f538507ef4e981bf4ed9
Amra <amrabano2003@gmail.com>
2024-10-23 15:37:15 +0530

    API Url change

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api";
+const API_URL = "https://bookshelf-yczp.onrender.com/api";
 
 // Function to signup 
 export const signupUser = async (username, email, password) => {
